680a72ae8826ef340c253065eff83f8442013071
Pavlinov Aleksandr <pandom@yandex.ru>
2023-09-18 23:49:24 +0300

    load_notes

--- notes.py
+++ notes.py
@@ -4,3 +4,10 @@ import datetime
 
 # Путь к файлу для хранения заметок
 FILE_PATH = "notes.json"
+
+def load_notes():
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH, "r") as file:
+            return json.load(file)
+    else:
+        return []
