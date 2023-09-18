2caeeddb2b045468ffa9d99b5333d76e2c09d6c6
Pavlinov Aleksandr <pandom@yandex.ru>
2023-09-19 00:00:03 +0300

    создание

--- notes.py
+++ notes.py
@@ -15,3 +15,15 @@ def load_notes():
 def save_notes(notes):
     with open(FILE_PATH, "w", encoding="utf-8") as file:
         json.dump(notes, file, indent=4, ensure_ascii=False)
+
+def create_note(title, body):
+    note = {
+        "id": len(notes) + 1,
+        "title": title,
+        "body": body,
+        "created_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "updated_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    notes.append(note)
+    save_notes(notes)
+    print("Заметка создана успешно!")
